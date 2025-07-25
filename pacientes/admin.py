bf56434798acd75fb612a6a4a8783ea0b32c1f39
Cristian Rozas <cristianrozas.cr@gmail.com>
2025-07-25 09:18:08 -0400

    se modifica el archivo admin

--- pacientes/admin.py
+++ pacientes/admin.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Mascota, Consulta, Vacuna
+from .models import Mascota, Consulta, Vacuna, ServicioPeluqueria
 
 admin.site.register(Mascota)
 admin.site.register(Consulta)
 admin.site.register(Vacuna)
+admin.site.register(ServicioPeluqueria)
